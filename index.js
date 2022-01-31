c271e6188716380153adce09de1c29d05cc77c26
Maggie Finnegan <wisner.maggiel@gmail.com>
2022-01-30 22:45:17 -0800

    added all ADD functions and prompts

--- index.js
+++ index.js
@@ -172,10 +172,150 @@ function addDepartment() {
 };
 
 //ADD Role
-function addRole() {}
+function addRole() {
+  const sqlRole= `select roles.*, departments.name as department_name from roles LEFT JOIN departments on roles.department_id;`;
+  db.query(sqlRole, (err, rows) => {
+    if (err) throw err;
+    console.table('List of active roles:', rows);
+  });
+  const sqlDept = `select all from departments`;
+  db.query(sqlDept, (err, rows) => {
+    console.table('List of departments for reference:', rows)
+    inquirer.prompt ([
+      {
+        type: 'input',
+        name: 'newTitle',
+        message: 'Enter the new role title.',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log ('Please enter a valid title.');
+            return false;
+          }
+        }
+      },
+      {
+         type: 'input',
+        name: 'newSalary',
+        message: 'Enter salary for this new role.',
+        validate: numInput => {
+          if (numInput) {
+            return true;
+          } else {
+            console.log ('Please enter a valid amount.');
+            return false;
+          }
+        }
+      },
+      {
+       type: 'input',
+        name: 'newDeptId',
+        message: 'Enter the department by ID associated with this new role.',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log ('Please enter a valid id.');
+            return false;
+          }
+        }
+      }
+    ])
+    .then (function(data) {
+      const newTitle = data.newTitle;
+      const newSalary = data.newSalary;
+      const newDept = data.newDept;
+      const sql = `INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)`;
+            const params = [newTitle, newSalary, newDept];
+            db.query(sql, params, (err, rows) => {
+                if (err) throw err;
+                console.log(`You added ${newTitle} to the database.`);
+        viewRoles();
+          });
+      })
+    })
+  };
 
 //ADD Employee
-function addEmployee() {}
+function addEmployee() {
+  const sqlEmployee= `select employees.*, roles.title as role_title, roles.salary, department_id, departments.name as department_name from employees LEFT JOIN roles ON employees.role_id = roles.id LEFT JOIN departments ON roles.department_id = departments.id;`;
+  db.query(sqlEmployee, (err, rows) => {
+    if (err) throw err;
+    console.table('Current employees:', rows);
+  });
+  const sqlRoles = `select roles.*, departments.name AS department_name from roles LEFT JOIN departments ON roles.department_id = departments.id;`;
+  db.query(sqlRoles, (err, rows) => {
+    console.table('List of all Roles:', rows)
+    inquirer.prompt ([
+      {
+        type: 'input',
+        name: 'newFirstName',
+        message: 'Enter employee\'\s first name.',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log ('Please enter a name.');
+            return false;
+          }
+        }
+      },
+      {
+         type: 'input',
+        name: 'newLastName',
+        message: 'Enter employee\'\s last name.',
+        validate:nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log ('Please enter a name.');
+            return false;
+          }
+        }
+      },
+      {
+       type: 'input',
+        name: 'newRoleId',
+        message: 'Enter employee\'\s new role by ID.',
+        validate: numInput => {
+          if (numInput) {
+            return true;
+          } else {
+            console.log ('Please enter a valid id.');
+            return false;
+          }
+        }
+      },
+      {
+       type: 'input',
+        name: 'newManagerId',
+        message: 'Enter employee\'\s manager by ID.',
+        validate: numInput => {
+          if (numInput) {
+            return true;
+          } else {
+            console.log ('Please enter a valid id.');
+            return false;
+          }
+        }
+      }
+    ])
+    .then (function(data) {
+      const newFirstName = data.newFirstName;
+      const newLastName = data.newLastName;
+      const newRoleId = data.newRoleId;
+      const newManagerId = data.newManagerId;
+      const sql = `INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
+            const params = [newFirstName, newLastName, newRoleId, newManagerId];
+            db.query(sql, params, (err, rows) => {
+                if (err) throw err;
+                console.log(`You added ${newFirstName} ${newLastName} to the database.`);
+        viewEmployees();
+          });
+      })
+  });
+}
 
 //UPDATE Role
 function updateRole() {}
